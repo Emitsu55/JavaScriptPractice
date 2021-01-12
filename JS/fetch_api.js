e095a00ee51891c5087e7fae7b04eb762637faea
Emitsu55 <emitsu.studios@gmail.com>
2021-01-12 19:10:49 -0300

    fetch_api

--- JS/fetch_api.js
+++ JS/fetch_api.js
@@ -1,23 +1,31 @@
 // Fetch API
 
 
-function obtenerEmpleados() {
+async function obtenerEmpleados() {
 
     const archivo = 'JS/empleados.json';
 
+    /*
     fetch(archivo)
         .then(resultado => resultado.json())
-
         .then(datos => {
-            // console.log(datos)
-
 
             //console.log(datos.empleados); (una forma de acceder a los datos)
 
             //con object destructuring:
             const { empleados } = datos;
             console.log(empleados);
+            empleados.forEach( empleado => {
+                console.log(empleado);
+            })
         })
+        */
+
+        //usando await
+        const resultado = await fetch(archivo);
+        const datos = await resultado.json();
+        console.log(datos);
+
 
 }
 
